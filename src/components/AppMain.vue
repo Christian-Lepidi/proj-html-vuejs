e7ce173c8bd98af0a08dc6dcd3f433073174de13
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-20 15:53:40 +0100

    feat: rename some img

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,20 @@
 <script>
 import AppDescription from "./AppDescription.vue";
 import AppAboutUs from "./AppAboutUs.vue";
+import AppProjects from "./AppProjects.vue";
 
 export default {
   data() {
     return {};
   },
-  components: { AppDescription, AppAboutUs },
+  components: { AppDescription, AppAboutUs, AppProjects },
 };
 </script>
 
 <template>
   <app-description />
   <app-about-us />
+  <app-projects />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppProjects.vue
@@ -0,0 +1,106 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <section class="main-container">
+    <div class="upper-container">
+      <div class="text-box">
+        <div>
+          <div class="line"></div>
+          <span class="slogan">PROJECTS</span>
+          <div class="big-text">OUR EXPERT TRUSTED CONSULTANS HELP CLIENT</div>
+        </div>
+      </div>
+      <div class="arrows-box">
+        <div>
+          <i class="fas fa-arrow-left arrows"></i>
+          <i class="fas fa-arrow-right arrows"></i>
+        </div>
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="card">
+        <img src="../assets/img/" alt="" />
+      </div>
+      <div class="card"></div>
+      <div class="card"></div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.main-container {
+  width: 100%;
+  height: 650px;
+  background-color: #1f1e38;
+
+  .upper-container {
+    width: 80%;
+    height: 250px;
+    margin: auto;
+    display: flex;
+
+    .text-box {
+      width: 50%;
+      height: 250px;
+      display: flex;
+      align-items: center;
+
+      .line {
+        height: 2px;
+        width: 30px;
+        background-color: #39ffbf;
+        display: inline-block;
+      }
+
+      .slogan {
+        color: white;
+        margin-left: 15px;
+      }
+
+      .big-text {
+        color: white;
+        font-size: 30px;
+      }
+    }
+
+    .arrows-box {
+      width: 50%;
+      height: 250px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      .arrows {
+        color: white;
+        background-color: #333333;
+        padding: 10px;
+      }
+
+      .fa-arrow-right {
+        margin-left: 4px;
+      }
+    }
+  }
+
+  .card-container {
+    width: 80%;
+    height: 350px;
+    margin: auto;
+    background-color: orange;
+    display: flex;
+    gap: 10px;
+
+    .card {
+      height: 350px;
+      width: calc(100% / 3 - 10px);
+      background-color: aquamarine;
+    }
+  }
+}
+</style>
